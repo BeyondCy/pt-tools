3def044399baa5571ec71acd3d47121dedec52c9
liviu <liviu@liviu-ThinkPad-T420>
2013-03-18 11:04:10 +0100

    - corrected intermixed outputs

--- smtp-find.sh
+++ smtp-find.sh
@@ -3,11 +3,17 @@
 if [ -z "$1" ]; then
     echo "[*] Simple SMTP discovery script"
     echo "[*] Usage : $0 <hosts_file>"
-    echo "[*] Example : $0 hosts.txt"
+    echo "[*] Example : $0 hosts.txt out.txt"
     exit 0
 fi
 
 hosts=$1
 for ip in `cat $hosts`;do
-	nmap -sS -P0 -p 25 -oG - $ip | grep open &
+	# Avoid intermingled outputs
+	nmap -sS -P0 -p 25 -oG - $ip | grep open >$ip.res &
 done
+
+# Manually concatenate the results
+# cat *.res > smtp_servers.txt
+# rm -rf *.res
+# cat smtp_servers.txt  | cut -d " " -f 2
